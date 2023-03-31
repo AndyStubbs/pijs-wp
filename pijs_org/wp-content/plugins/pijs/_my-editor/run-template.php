23da25311b4620ae9ce36a73ba0708ed84692978
AndysCodeVault <andy.stubbs@protonmail.com>
2023-03-31 11:20:41 -0600

    Stopping code from being run if it is not from pijs.org editor or playground

--- pijs_org/wp-content/plugins/pijs/_my-editor/run-template.php
+++ pijs_org/wp-content/plugins/pijs/_my-editor/run-template.php
@@ -1,3 +1,14 @@
+<?php
+	$allowed_domains = array( 'localhost', 'pijs.org', 'www.pijs.org' );
+
+	// Get the referring domain from the HTTP_REFERER header
+	$referer = isset($_SERVER['HTTP_REFERER']) ? parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST) : null;
+
+	// Check if the referring domain is in the list of allowed domains
+	if (!$referer || !in_array( $referer, $allowed_domains ) ) {
+		die( 'You can only access this page through pijs.org editor or playground.' );
+	}
+?>
 <!DOCTYPE html>
 <html lang="en">
 	<head>
